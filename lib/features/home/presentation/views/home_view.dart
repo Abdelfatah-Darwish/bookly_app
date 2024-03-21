ee95b47c38e5b925f7a5ef55f6981f69fc19d014
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-03-21 23:54:08 +0200

    add custom app bar to home view screen

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,11 +6,8 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home View'),
-      ),
-      body: Container(),
+    return const Scaffold(
+      body: HomeViewBody(),
     );
   }
 }
