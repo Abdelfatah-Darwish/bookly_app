6f5f06d79365660184e554302ed8534fa045afd4
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-03-24 14:28:57 +0200

    create app styles

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -5,7 +5,6 @@ import 'package:flutter/widgets.dart';
 
 import 'custom_app_bar.dart';
 import 'featured_box_list_view.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -22,7 +21,7 @@ class HomeViewBody extends StatelessWidget {
           SizedBox(height: MediaQuery.of(context).size.height * 0.056),
           const Text(
             'Best Seller',
-            style: Styles.titleMediam,
+            style: Styles.textStyle18,
           ),
           const BestSellerListViewItem(),
         ],
@@ -30,5 +29,3 @@ class HomeViewBody extends StatelessWidget {
     );
   }
 }
-
-
