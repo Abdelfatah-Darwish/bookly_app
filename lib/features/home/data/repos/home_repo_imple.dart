b35d1ecbe9a6dee20ff1b1d6a4203c5c719f7683
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-04-02 15:21:31 +0200

    create api service

---
+++ lib/features/home/data/repos/home_repo_imple.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+ class HomeRepoImple implements HomeRepo{
+  @override
+  Future<Either<Failure, BookModel>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, BookModel>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+
+}
